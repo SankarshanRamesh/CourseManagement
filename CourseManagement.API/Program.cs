2f5a7c9ae950904514c084cc9c743e5b9b552ea9
Sankarshan Ramesh <sankarshanr@gmail.com>
2024-10-26 17:17:20 +0530

    aadded ef core and scaffolded the entities

--- CourseManagement.API/Program.cs
+++ CourseManagement.API/Program.cs
@@ -1,6 +1,26 @@
+using CourseManagement.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
+
+//DB configuration goes here
+//Tips, if you want to see what are paramters we can enable here but it shows all sensitive data
+//so only used for development purpose should not go to PRODUCTION!
+builder.Services.AddDbContextPool<OnlineCourseDbContext>(options =>
+{
+    options.UseSqlServer(
+        configuration.GetConnectionString("DbContext"),
+    provideroptions => provideroptions.EnableRetryOnFailure()
+
+    );
+    options.EnableSensitiveDataLogging();
+    options.EnableDetailedErrors();
+
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
